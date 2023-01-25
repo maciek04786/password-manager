709e90faac8651618e45ff59ef627d790db11b53
Maciek S <105375946+maciek04786@users.noreply.github.com>
2023-01-25 16:53:41 +0000

    Change button layout

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def generate_password():
 # ---------------------------- SAVE ACCOUNT ------------------------------- #
 def save_account():
 
-    website = website_entry.get()
+    website = website_entry.get().lower()
     email = email_entry.get()
     password = password_entry.get()
     new_data = {
@@ -79,9 +79,9 @@ def clear_entries():
     password_entry.delete(0, END)
 
 
-# --------------------------- FIND PASSWORD --------------------------- #
+# --------------------------- FIND ACCOUNT --------------------------- #
 def find_password():
-    website = website_entry.get()
+    website = website_entry.get().lower()
     try:
         with open("data.json", "r") as data_file:
             data = json.load(data_file)
@@ -111,33 +111,29 @@ canvas.grid(column=1, row=0)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky="w")
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky="w")
 
 # Entries
-website_entry = Entry(width=21)
-website_entry.grid(row=1, column=1, sticky="we", padx=5)
+website_entry = Entry(width=32)
+website_entry.grid(row=1, column=1, sticky="w", padx=5)
 website_entry.focus()
 email_entry = Entry(width=32)
-email_entry.grid(row=2, column=1, pady=5)
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1, padx=5, pady=5, sticky="we")
+email_entry.grid(row=2, column=1, sticky="w", padx=5, pady=5)
+password_entry = Entry(width=32)
+password_entry.grid(row=3, column=1, sticky="w", padx=5)
 
 # Buttons
-search_button = Button(text="Search", width=14, command=find_password)
-search_button.grid(row=1, column=2, columnspan=2, sticky="w")
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2, columnspan=2, sticky="w")
-add_button = Button(text="Add account", width=27, command=save_account)
-add_button.grid(row=4, column=1, sticky="w")
-clear_button = Button(text="Clear all fields", width=14, command=clear_entries)
-clear_button.grid(row=4, column=2, padx=5, columnspan=2, sticky="w")
-save_email_button = Button(text="Save", width=6, command=save_email)
-save_email_button.grid(row=2, column=2, sticky="w")
-load_email_button = Button(text="Load", width=6, command=load_email)
-load_email_button.grid(row=2, column=3, sticky="w")
+search_button = Button(text="Search", width=15, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", width=15, command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add account", width=18, command=save_account)
+add_button.grid(row=4, column=1, pady=5)
+clear_button = Button(text="Clear entries", width=15, command=clear_entries)
+clear_button.grid(row=4, column=2)
 
 window.mainloop()
